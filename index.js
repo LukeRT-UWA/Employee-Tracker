9a79207a81552d30b909f8f58bdf39b87b92f8d1
Luke Thompson <16175583curtin@gmail.com>
2021-08-14 16:53:52 +0800

    code refactor for efficiency

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const {employeeOperations} = require('./lib/employee')
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'Password01',
+      database: 'films_db'
+    },
+    console.log(`Connected to the courses_db database.`)
+  );
+
+
+const operations = {
+    ...employeeOperations,
+    
+    
+    "Exit App": process.exit
+}
+
+
+function whatWouldLikeToDo(){
+    inquirer
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "Would you like to do?",
+      choices: Object.keys(operations)
+    })
+    .then( ({ task })=> operations[task]() )
+    .then(() => whatWouldLikeToDo());
+}
 
 function main() {
 
-    inquirer
-      .prompt({
-        type: "list",
-        name: "task",
-        message: "Would you like to do?",
-        choices: [
-          "View Employees"]
-      })
-      .then(function ({ task }) {
-        switch (task) {
-          case "View Employees":
-            viewEmployee();
-            break;
-        }
-      });
+   whatWouldLikeToDo();
   }
 
   main()
+
